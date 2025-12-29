a2665872b3ba45df0f21db1816598f15ceb708c7
Geoff White <l0r3zz@gmail.com>
2025-12-28 22:24:58 -0800

    minor changes

--- scripts/bb.sh
+++ scripts/bb.sh
@@ -1,3 +1,4 @@
+# make sure you export MD_PATH, PDF_PATH, OUTPUT_DIR 
 python scripts/build_book.py -s content -d SLOBLACKSWAN -v "v0.xx" -m MANIFEST.md
 python scripts/insert_pagebreaks.py --overwrite $MD_PATH
 doctoc ${MD_PATH} --title "## Table of Contents"
